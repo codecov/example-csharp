f279ef960f79d3db23ff5f893ffebe9a6e70150e
freelancingmikey <freelancing.mikey@gmail.com>
2016-08-22 22:05:25 +0530

    Initial Checkin of whole directory.

---
+++ MyUnitTests/MyTargetClassTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestTargetProject;
+
+namespace MyUnitTests
+{
+    [TestClass]
+    public class MyTargetClassTests
+    {
+        [TestMethod]
+        public void VerifySummation()
+        {
+            int intFirstNumber = 15, intsecondNumber = 10;
+            MyTargetClass objTargetClass = new MyTargetClass();
+            int intResult = objTargetClass.AddNumbers(intFirstNumber, intsecondNumber);
+            Assert.AreEqual(15 + 10, intResult, "Summation method is not written well.");
+        }
+
+        [TestMethod]
+        public void VerifySubtraction()
+        {
+            int intFirstNumber = 15, intsecondNumber = 10;
+            MyTargetClass objTargetClass = new MyTargetClass();
+            int intResult = objTargetClass.SubractNumbers(intFirstNumber, intsecondNumber);
+            Assert.AreEqual(15 - 10, intResult, "Subtraction method is not written well.");
+        }
+    }
+}
---
+++ UnitTestTargetProject/MyTargetClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestTargetProject
+{
+    public class MyTargetClass
+    {
+        public int AddNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber + pintSecondNumber;
+        }
+        public int SubractNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber - pintSecondNumber;
+        }
+        public int DivideNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber / pintSecondNumber;
+        }
+    }
+}
